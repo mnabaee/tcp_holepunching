3f46e614550331da13e1173d22bb709a6fe5e8cb
Mahdy En <mahdy.nabaee@gmail.com>
2015-03-25 18:59:31 -0400

    third commit: ready to use

--- prep_.sh
+++ prep_.sh
@@ -1,5 +1,2 @@
 g++ server.cc -o server -lpthread
 g++ client.cc -o client -lpthread
-scp -i ../mahdy.pem -r ./ ubuntu@54.68.127.121:~/pp/
-ssh -i ../mahdy.pem ubuntu@54.68.127.121 'g++ ~/pp/server.cc -o ~/pp/server -lpthread'
-ssh -i ../mahdy.pem ubuntu@54.68.127.121 'g++ ~/pp/client.cc -o ~/pp/client -lpthread'
